d5e6a6b3f9466b20f0935f0c5fa93cef1df27aef
Jacky Jiang <765643729@163.com>
2025-08-17 12:46:32 +0800

    临时提交

--- ToDo/MainWindow.xaml.cs
+++ ToDo/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ToDo;
 /// <summary>
@@ -18,5 +11,34 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void TaskTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            var textBox = sender as TextBox;
+            if (textBox == null)
+            {
+                return;
+            }
+
+            if (Keyboard.Modifiers == ModifierKeys.Shift)
+            {
+                Console.WriteLine("Shift换行");
+                // Shift+Enter：插入换行
+                int caret = textBox.CaretIndex;
+                textBox.Text = textBox.Text.Insert(caret, "\n");
+                textBox.CaretIndex = caret + 1;
+                e.Handled = true;
+            }
+            else
+            {
+                Console.WriteLine("提交内容");
+                // Enter：提交
+                MessageBox.Show("提交内容：" + textBox.Text);
+                e.Handled = true; // 阻止默认换行
+            }
+        }
     }
 }
